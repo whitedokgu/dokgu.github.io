a5fc307d65a040dea69bd99af4ebf89dc1422558
whitedokgu <80078375+whitedokgu@users.noreply.github.com>
2021-03-31 12:40:07 +0900

    Add files via upload

---
+++ 메뉴바/menu_javascript.js
@@ -0,0 +1,9 @@
+
+function toggleMenu() {
+    let navigation = document.querySelector('.navi');
+    let toggle = document.querySelector('.toggle');
+    navigation.classList.toggle('active');
+    toggle.classList.toggle('active');
+
+}
+
