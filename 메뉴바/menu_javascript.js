1ab56fb5a4d8a4290b295e4c7df05e902da508e5
whitedokgu <80078375+whitedokgu@users.noreply.github.com>
2021-04-08 16:09:44 +0900

    Delete 메뉴바 directory

--- 메뉴바/menu_javascript.js
+++
@@ -1,9 +0,0 @@
-
-function toggleMenu() {
-    let navigation = document.querySelector('.navi');
-    let toggle = document.querySelector('.toggle');
-    navigation.classList.toggle('active');
-    toggle.classList.toggle('active');
-
-}
-
