161f2e607fea5ba3f9cab741638cc213b98dc2bc
whitedokgu <80078375+whitedokgu@users.noreply.github.com>
2021-06-02 10:53:11 +0900

    Delete 킹덤 directory

--- 킹덤/js/1.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (e) {
-    /* a요소를 클릭 했을 시 */
-    $('.nav-link a').click(function () {
-        /* iframe 요소의 src 속성값을 a 요소의 data-url 속성값으로 변경 */
-        $('#iframe').attr('src', $(this).attr('data-url'));
-    })
-})
-
